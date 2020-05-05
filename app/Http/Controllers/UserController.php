c691801d36b26adc0491c91c6f3fb80f7894da04
Victor Guimaraes <victor.gmr@hotmail.com>
2020-05-05 16:37:42 -0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    public function index()
+    {
+        return response()->json(User::get(), 200);
+    }
+
+    public function store(Request $request)
+    {
+        return response(User::create($request->all()), 201);
+    }
+
+    public function show(User $user)
+    {
+        return response()->json(User::find($user->id), 200);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+        return response()->json($user, 200);
+    }
+
+    public function destroy(User $user)
+    {
+        return response($user->delete(), 200);
+    }
+}
